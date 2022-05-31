83504142eda2b5bc45d2fd5ef18e9ec0e987eb41
sharansharma94 <sharansharma19@gmail.com>
2022-05-31 18:37:57 +0530

    docker compose file added for postgres and api

--- backend/run.sh
+++ backend/run.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
-docker build -f dockerFile . -t go-gin
-docker run -i -t -p 8080:8080 go-gin
+# docker build -f dockerFile . -t go-gin
+# docker run -i -t -p 8080:8080 go-gin
+
+docker-compose -f docker-compose.yml up --build
